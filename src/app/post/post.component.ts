fdd26eb8a29cbe0c8a1e8befa35c2f6ac4833b80
jmgloria07 <joshua.gloria07@gmail.com>
2020-09-19 20:39:29 +0800

    Support POST to Gracenote API

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -11,11 +11,27 @@ export class PostComponent implements OnInit {
 
   mOpenings:any;
 
+  mFormGroup = new FormGroup({
+    "displayText": new FormControl('', [
+      Validators.required,
+      Validators.minLength(1)
+    ]),
+    "opening": new FormControl('', [
+      Validators.required
+    ])
+  });
+
   constructor(private graceNoteApi:GracenoteApiService){ 
+  
   }
 
   ngOnInit(): void {
     this.graceNoteApi.getOpenings().subscribe(data => this.mOpenings = data);
+  }
+
+  createGrace(): void {
+    console.log("entered create grace");
+    this.graceNoteApi.postGrace(this.mFormGroup.value);
   }
 
 }
