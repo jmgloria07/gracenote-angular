60a75194bbd02be58147e86538b3ee3c8b7653a4
jmgloria07 <joshua.gloria07@gmail.com>
2020-09-19 09:01:41 +0800

    Add Angular Material and Service imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+import { GracenoteApiService } from './gracenote-api.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { PostComponent } from './post/post.component';
+import { FeedComponent } from './feed/feed.component';
+import { GraceComponent } from './feed/grace/grace.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    PostComponent,
+    FeedComponent,
+    GraceComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule
   ],
-  providers: [],
+  providers: [GracenoteApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
